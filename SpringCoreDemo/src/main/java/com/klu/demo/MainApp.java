f414bbed90efa2cec69fbc114bd491fdda1ba06e
sriharsha8076 <2400031017@kluniversity.in>
2026-01-07 10:39:19 +0530

    Initial commit

---
+++ SpringCoreDemo/src/main/java/com/klu/demo/MainApp.java
@@ -0,0 +1,17 @@
+package com.klu.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		Car car=(Car)context.getBean("car");
+		car.drive();
+		
+		Bike bike=(Bike)context.getBean("bike");
+		bike.ride();
+	}
+
+}
